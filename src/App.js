8b5c79a1fd317b8192469362100a450732894f76
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-25 22:09:26 -0600

    Create App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import Header from "./Header/Header";
+import Answer from "./Answer/Answer";
+import QuestionCard from "./QuestionCard/QuestionCard";
 
-function App() {
+class App extends Component {
+  constructor(){
+    super()
+      this.state = {
+        data: [],
+        questions: [],
+        currentQuestion: '',
+        correctAnswer: '',
+        answers: [],
+        incorrectAnswers: []
+      }
+    }
+
+componentDidMount() {
+  fetch("https://opentdb.com/api.php?amount=20&category=20&difficulty=medium&type=multiple")
+    .then(response => response.json())
+    .then(triviaData => {
+      const questions = triviaData.results.map(triviaItem => {
+        return triviaItem.question
+      })
+      const answers = [...triviaData.results[0].incorrect_answers, triviaData.results[0].correct_answer]
+      this.setState({data: triviaData.results, questions: questions, currentQuestion: questions[0], correctAnswer: triviaData.results[0].correct_answer, answers: answers})
+    })
+    .catch(error => console.log("error"))
+}
+
+onSubmit(answer) {
+  
+}
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <Header/>
+     <main className="container">
+        {!this.state.data && <p>Loading...</p>}
+        <QuestionCard questionInfo={this.state} onSubmit={this.onSubmit}/> 
+        <Answer/>
+     </main>
+    </>
+  )
+}
 }
 
 export default App;
