d2a020652c65266d5f5d00273cc770084ae5ed3d
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-02 20:01:35 -0600

    Added error handling for invalid input

--- src/Answer/Answer.js
+++ src/Answer/Answer.js
@@ -2,25 +2,36 @@ import React, { Component } from "react";
 import "./Answer.css";
 
 class Answer extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
       answer: '',
+      isCorrect: false,
+      invalid: false,
     }
   }
   handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({[name]: value})
+   
   }
 
   handleSubmit = (event) => {
-    
+    event.preventDefault();
+    const validAnswers = ["a", "b", "c", "d", "A", "B", "C", "D"]
+    if(validAnswers.includes(this.state.answer)) {
+      this.setState({answer: "", invalid: false})
+      this.props.onSubmit(this.state.answer); 
+   } else {
+     this.setState({invalid: true})
+   }
   }
 
-  render() {
-    console.log(this.state); 
+  render() { 
+    console.log(this.state)
     return (
       <form className="answer-container" onSubmit={this.handleSubmit}>
+         {this.state.invalid && <p>Please choose A, B, C, or D</p>}
         <label>Your Answer</label>
         <input className="answer-input" type="text" value={this.state.answer} name="answer" onChange={this.handleChange}/>
         <button type="submit" className="answer-button">Check Answer</button>
