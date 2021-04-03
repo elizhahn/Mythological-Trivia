95f28ac738c85016a1bdc0b24c3edb5a9d03762f
Elizabeth Hahn <elizhahn87@gmail.com>
2021-04-02 20:02:23 -0600

    Check answer and update state functinality

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         questions: [],
         currentQuestion: '',
         correctAnswer: '',
-        answers: [],
+        answers: {},
         incorrectAnswers: []
       }
     }
@@ -24,25 +24,54 @@ componentDidMount() {
       const questions = triviaData.results.map(triviaItem => {
         return triviaItem.question
       })
+      const keys = ["a", "b", "c", "d"]
       const answers = [...triviaData.results[0].incorrect_answers, triviaData.results[0].correct_answer]
-      this.setState({data: triviaData.results, questions: questions, currentQuestion: questions[0], correctAnswer: triviaData.results[0].correct_answer, answers: answers})
+      const answerKey = keys.reduce((answerList, key, i) => {
+        answerList[key] = answers[i]
+        return answerList
+      }, {})
+      this.setState({data: triviaData.results, questions: questions, currentQuestion: questions[0], correctAnswer: triviaData.results[0].correct_answer, answers: answerKey})
     })
     .catch(error => console.log("error"))
 }
 
-onSubmit(answer) {
-  
+generateNewQuestion = (index) => {
+  const newQuestion = this.state.questions[index + 1];
+  const newCorrectAnswer = this.state.data[index + 1].correct_answer;
+  const keys = ["a", "b", "c", "d"]
+  const answers = [...this.state.data[index + 1].incorrect_answers, this.state.data[index + 1].correct_answer]
+  const answerKey = keys.reduce((answerList, key, i) => {
+    answerList[key] = answers[i]
+    return answerList
+  }, {})
+
+  return {question: newQuestion, correctAnswer: newCorrectAnswer, answers: answerKey}
 }
 
 
-render() {
+
+onSubmit = (answer) => {
+  const questionIndex = this.state.questions.indexOf(this.state.currentQuestion)
+  const question = this.generateNewQuestion(questionIndex)
+  console.log(this.state)
+  if(this.state.answers[answer] === this.state.correctAnswer) {
+    console.log("test")
+    this.setState({currentQuestion: question.question, correctAnswer: question.correctAnswer, answers: question.answers})
+} else {
+    this.setState({currentQuestion: question.question, correctAnswer: question.correctAnswer, answers: question.answers, incorrectAnswers: [...this.state.incorrectAnswers, answer]})
+  }
+  
+}
+
+render() { 
+  console.log(this.state)
   return (
     <>
      <Header/>
      <main className="container">
         {!this.state.data && <p>Loading...</p>}
         <QuestionCard questionInfo={this.state} onSubmit={this.onSubmit}/> 
-        <Answer/>
+        <Answer onSubmit={this.onSubmit}/>
      </main>
     </>
   )
